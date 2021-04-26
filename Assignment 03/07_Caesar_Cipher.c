55e895391dc7cd558ea123bfd6dac168bc82dc3d
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-04-26 10:10:18 +0530

    Update 07_Caesar_Cipher.c

--- Assignment 03/07_Caesar_Cipher.c
+++ Assignment 03/07_Caesar_Cipher.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-  char x;
+  char x, encrypt;
+  int shift_key = 3;
 
   printf("Enter a Character: ");
   scanf("%c", &x);
 
-  printf("Encrpted Code : %c", x+3);
+  encrypt = x + shift_key;
+
+  printf("Encrypted Code : %c", encrypt);
 
   return 0;
 }
